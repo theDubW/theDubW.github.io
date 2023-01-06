04de4ba32ebc6a3cad39f86b5bd92cecc548ff58
theDubW <maxberniew@gmail.com>
2023-01-05 18:47:52 -0600

    basic react project

---
+++ ReactProject/react-tailwindcss-portfolio-master/src/data.js
@@ -0,0 +1,65 @@
+export const projects = [
+  {
+    title: "UT Course Scheduler",
+    subtitle: "Chrome Extension For UT Students",
+    description:
+      "An extension which cuts the hassle of registering for classes by doing it all in < 1 second. "
+      + "Enter all the courses you want and your registration time, and"
+      +" the stress of signing up for classes ends. Made from the ground up in JS + HTML + CSS.",
+    image: "./ut_course_scheduler.gif",
+    link: "https://github.com/loganvaz/ChromeCoursePlanner",
+  },
+  {
+    title: "MaxTrade",
+    subtitle: "Stock and Options Trading Bot For Robinhood",
+    description:
+      "A program which allows users to create automatic buy and sell "
+      +"orders for stocks and specific options contracts they want in an intuitive UI. Built from "
+      +"scratch in Python leveraging the Robin Stocks Library.",
+    image: "./MaxTrade_demo.png",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Ball Craze",
+    subtitle: "IOS Game",
+    description:
+      "A fun mobile game I developed in C++ using the open source cocos-2dx library. Collect balls "
+      +"and gems, unlock paddles and backgrounds, and compete to get the highest score.",
+    image: "./ballcraze_demo.gif",
+    link: "https://www.producthunt.com/products/ball-craze",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "GraphQL",
+  "Material UI",
+];
