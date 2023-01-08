2a9bac0e0316e8dd797e7f4f4631a6182cfa92bb
theDubW <maxberniew@gmail.com>
2023-01-08 00:38:13 -0600

    Updates

---
+++ precache-manifest.033243a2c37a1b56f5045bbaeb78c879.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "20b30965724da9ef0fa9",
+    "url": "/theDubW.github.io/static/css/main.9a022c5f.chunk.css"
+  },
+  {
+    "revision": "20b30965724da9ef0fa9",
+    "url": "/theDubW.github.io/static/js/main.0ae1537b.chunk.js"
+  },
+  {
+    "revision": "941a71808c62e88d312e",
+    "url": "/theDubW.github.io/static/js/runtime~main.81afb4cb.js"
+  },
+  {
+    "revision": "349c03dcf6f6fd3cce38",
+    "url": "/theDubW.github.io/static/js/2.0043d9ac.chunk.js"
+  },
+  {
+    "revision": "89c0202a200987b48f7c6e408d817718",
+    "url": "/theDubW.github.io/index.html"
+  }
+];
