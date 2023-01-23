08436aed3cdd66b942847ffdd43866449b1279f8
theDubW <maxberniew@gmail.com>
2023-01-23 00:45:07 -0600

    Updates

---
+++ precache-manifest.c1828cd85fa050144319c5d986c28751.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4889195b739e320751b1",
+    "url": "/static/css/main.02a09ce9.chunk.css"
+  },
+  {
+    "revision": "4889195b739e320751b1",
+    "url": "/static/js/main.f4b31a8a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b0d6060d5184aa240f21",
+    "url": "/static/js/2.5ffd9824.chunk.js"
+  },
+  {
+    "revision": "d44b24e891c870fa038d071274040733",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.67eafa536cd9e5f022735ec4d261f770.js"
+  "/precache-manifest.c1828cd85fa050144319c5d986c28751.js"
 );
 
 workbox.clientsClaim();
